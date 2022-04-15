e98ee10d6710c6c2f8f800542f7c39c2e6c1bd9a
Tez <tez.park@sendbird.com>
2022-04-15 12:28:49 +0900

    [ADD] added NotificationServiceExtension. [ADD] added call the `markAsDelivery` logic when did receive notification.

---
+++ Sample/NotificationService/NotificationService.swift
@@ -0,0 +1,39 @@
+//
+//  NotificationService.swift
+//  NotificationService
+//
+//  Created by Tez Park on 2022/04/15.
+//  Copyright © 2022 SendBird, Inc. All rights reserved.
+//
+
+import UserNotifications
+import SendBirdSDK
+
+class NotificationService: UNNotificationServiceExtension {
+
+    var contentHandler: ((UNNotificationContent) -> Void)?
+    var bestAttemptContent: UNMutableNotificationContent?
+
+    override func didReceive(_ request: UNNotificationRequest, withContentHandler contentHandler: @escaping (UNNotificationContent) -> Void) {
+        self.contentHandler = contentHandler
+        bestAttemptContent = (request.content.mutableCopy() as? UNMutableNotificationContent)
+        
+        if let bestAttemptContent = bestAttemptContent {
+            // Modify the notification content here...
+            bestAttemptContent.title = "\(bestAttemptContent.title) [modified]"
+            SBDMain.markAsDelivered(withRemoteNotificationPayload: bestAttemptContent.userInfo) { (error) in
+                
+            }
+            contentHandler(bestAttemptContent)
+        }
+    }
+    
+    override func serviceExtensionTimeWillExpire() {
+        // Called just before the extension will be terminated by the system.
+        // Use this as an opportunity to deliver your "best attempt" at modified content, otherwise the original push payload will be used.
+        if let contentHandler = contentHandler, let bestAttemptContent =  bestAttemptContent {
+            contentHandler(bestAttemptContent)
+        }
+    }
+
+}
