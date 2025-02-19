6ef1610d086ffa2bdf9db46d9b979f10b2b25bdd
Tez Park <tez.park@sendbird.com>
2025-02-19 12:24:36 +0900

    Updated iOS deployment target to 13.0 - package settings

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 
 let package = Package(
     name: "SendbirdUIKit",
-    platforms: [.iOS(.v12)],
+    platforms: [.iOS(.v13)],
     products: [
         .library(
             name: "SendbirdUIKit",
